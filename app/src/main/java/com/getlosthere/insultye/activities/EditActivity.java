7867ab4e796961f81fe46f228d3ace0790a1b498
violetaria <terri.chu@outlook.com>
2016-12-11 09:42:58 -0500

    start of edit activity and layout

---
+++ app/src/main/java/com/getlosthere/insultye/activities/EditActivity.java
@@ -0,0 +1,15 @@
+package com.getlosthere.insultye.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.getlosthere.insultye.R;
+
+public class EditActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit);
+    }
+}
